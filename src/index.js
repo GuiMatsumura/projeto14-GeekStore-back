3d4ccd18e85acc1788634fc28ea7132702ff5eee
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-07-07 23:14:51 -0300

    initial setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
